3d5cf625bbfb4ddf5f9a92cb9f661df1e90e621f
jsuleyka <suleymjb15@hotmail.com>
2022-12-26 10:43:47 -0600

    adding spinners effect

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const boton = document.querySelector('#boton');
-const menu = document.querySelector('#menu');
+const spin = document.querySelector('#spin-container');
+const bodyBackground = document.querySelector('#body-background');
+bodyBackground.classList.toggle('hidden');
+spin.classList.toggle('hidden');
 
-boton.addEventListener('click', () => {
-    menu.classList.toggle('hidden')
-});
+const Spin = () => {
+    spin.classList.toggle('hidden');
+    bodyBackground.classList.toggle('hidden');
+}
+setTimeout(Spin,3000);
+
+spin.classList.toggle('hidden');
