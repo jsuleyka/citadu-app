30879fb58824281b134cbd3a7efb15f75eeaaa5a
jsuleyka <suleymjb15@hotmail.com>
2022-12-28 08:57:10 -0600

    fix imagese

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const spin = document.querySelector('#spin-container');
 const bodyBackground = document.querySelector('#body-background');
-bodyBackground.classList.toggle('hidden');
-spin.classList.toggle('hidden');
+// bodyBackground.classList.toggle('hidden');
+// spin.classList.toggle('hidden');
 
-const Spin = () => {
+// const Spin = () => {
+//     spin.classList.toggle('hidden');
+//     bodyBackground.classList.toggle('hidden');
+// }
+
+setTimeout(function(){
     spin.classList.toggle('hidden');
     bodyBackground.classList.toggle('hidden');
-}
-setTimeout(Spin,3000);
+},3000);
 
-spin.classList.toggle('hidden');
+// spin.classList.toggle('hidden');
+bodyBackground.classList.toggle('hidden');
+// bodyBackground.classList.toggle('hidden');
